f885c7f2a6c02b82c8008a4853865a38acb78f45
keplor <keplor@naver.com>
2020-05-04 18:52:00 +0900

    Add FIFO Page Replacement Algorithm created By Queue

--- src/main/java/com/page/FIFO/MainProcess.java
+++ src/main/java/com/page/FIFO/MainProcess.java
@@ -1,5 +1,8 @@
 package com.page.FIFO;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * FIFO Page Replacement Algorithm
  *
@@ -7,11 +10,37 @@ package com.page.FIFO;
  * @since 20.05.04
  */
 public class MainProcess {
+    private final Queue<Integer> pageTable = new LinkedList<>();
+    private final int frameSize;
+    private int miss;
 
-    public void execute() {
+    public MainProcess() {
+        this(3);
+    }
+
+    public MainProcess(int frameSize) {
+        this.frameSize = frameSize;
+    }
+
+    public void execute(int[] pages) {
+        for (int page : pages) {
+            usePage(page);
+        }
+    }
+
+    private void usePage(int page) {
+        if (pageTable.contains(page)) {
+            return;
+        }
+
+        if (pageTable.size() == frameSize) {
+            pageTable.poll();
+        }
+        pageTable.offer(page);
+        miss++;
     }
 
     public int getPageDefault() {
-        return 0;
+        return miss;
     }
 }
--- src/test/java/com/page/FIFO/FIFOTest.java
+++ src/test/java/com/page/FIFO/FIFOTest.java
@@ -24,16 +24,16 @@ public class FIFOTest {
     public void 예제1() {
         int[] pages = {8, 1, 2, 3, 1, 4, 1, 5, 3, 4, 1, 4, 3, 2};
 
-        process.execute();
+        process.execute(pages);
 
-        Assert.assertThat(process.getPageDefault(), is(10));
+        Assert.assertThat(process.getPageDefault(), is(11));
     }
 
     @Test
     public void 예제2() {
         int[] pages = {2, 3, 2, 1, 5, 2, 3, 5};
 
-        process.execute();
+        process.execute(pages);
 
         Assert.assertThat(process.getPageDefault(), is(6));
     }
