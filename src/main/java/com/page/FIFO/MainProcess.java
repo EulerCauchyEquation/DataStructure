0ed7b21e6abd4a37dd311bfb7b322b8f73c5c470
keplor <keplor@naver.com>
2020-05-04 18:41:44 +0900

    Add FIFO Page Replacement Algorithm test

---
+++ src/main/java/com/page/FIFO/MainProcess.java
@@ -0,0 +1,17 @@
+package com.page.FIFO;
+
+/**
+ * FIFO Page Replacement Algorithm
+ *
+ * @author 송훤출
+ * @since 20.05.04
+ */
+public class MainProcess {
+
+    public void execute() {
+    }
+
+    public int getPageDefault() {
+        return 0;
+    }
+}
