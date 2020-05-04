28a9021f8b177d06c9b8adfccf3f505c52da335d
keplor <keplor@naver.com>
2020-05-04 12:37:28 +0900

    Add LRU Algorithm test

---
+++ src/main/java/com/page/LRU/Cache.java
@@ -0,0 +1,19 @@
+package com.page.LRU;
+
+/**
+ * LRU Algorithm
+ *
+ * @author 송훤출
+ * @since 20.05.04
+ */
+public class Cache {
+    public Cache(int frameSize) {
+    }
+
+    public void execute(int[] pages) {
+    }
+
+    public int getPageDefault() {
+        return 0;
+    }
+}
---
+++ src/test/java/com/page/LRU/LRUTest.java
@@ -0,0 +1,40 @@
+package com.page.LRU;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ * LRU Algorithm : Least Recently Used Page Algorithm
+ *
+ * @author 송훤출
+ * @since 20.05.04
+ */
+public class LRUTest {
+    private Cache cache;
+
+    @Before
+    public void setUp() {
+        cache = new Cache(3);
+    }
+
+    @Test
+    public void LRU_예제1() {
+        int[] pages = {8, 1, 2, 3, 1, 4, 1, 5, 3, 4, 1, 4, 3, 2};
+
+        cache.execute(pages);
+
+        Assert.assertThat(cache.getPageDefault(), is(10));
+    }
+
+    @Test
+    public void LRU_예제2() {
+        int[] pages = {0, 1, 2, 3, 0, 1, 4, 0, 1, 2, 3, 4};
+
+        cache.execute(pages);
+
+        Assert.assertThat(cache.getPageDefault(), is(8));
+    }
+}
