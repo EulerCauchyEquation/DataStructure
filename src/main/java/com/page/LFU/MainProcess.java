d4b25e05b8b84502d4e63c8e706dd945557824d0
keplor <keplor@naver.com>
2020-05-04 19:14:03 +0900

    Add LFU Page Replacement Algorithm test

---
+++ src/main/java/com/page/LFU/MainProcess.java
@@ -0,0 +1,16 @@
+package com.page.LFU;
+
+/**
+ * LFU : Least Frequently Used
+ *
+ * @author 송훤출
+ * @since 20.05.04
+ */
+public class MainProcess {
+    public void execute(int[] pages) {
+    }
+
+    public int getPageDefault() {
+        return 0;
+    }
+}
---
+++ src/test/java/com/page/LFU/LFUTest.java
@@ -0,0 +1,40 @@
+package com.page.LFU;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ * LFU Page Replacement Test
+ *
+ * @author 송훤출
+ * @since 20.05.04
+ */
+public class LFUTest {
+    private MainProcess process;
+
+    @Before
+    public void setUp() {
+        process = new MainProcess();
+    }
+
+    @Test
+    public void 예제1() {
+        int[] pages = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1};
+
+        process.execute(pages);
+
+        Assert.assertThat(process.getPageDefault(), is(11));
+    }
+
+    @Test
+    public void 예제2() {
+        int[] pages = {2, 3, 1, 3, 1, 2, 4, 5};
+
+        process.execute(pages);
+
+        Assert.assertThat(process.getPageDefault(), is(6));
+    }
+}
