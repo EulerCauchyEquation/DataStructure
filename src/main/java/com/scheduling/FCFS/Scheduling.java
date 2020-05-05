a360837f31965141b8b86299d78e9d13dc3957b0
keplor <keplor@naver.com>
2020-05-05 18:04:34 +0900

    Add FCFS Scheduling created by FIFO

--- src/main/java/com/scheduling/FCFS/Scheduling.java
+++ src/main/java/com/scheduling/FCFS/Scheduling.java
@@ -1,8 +1,7 @@
 package com.scheduling.FCFS;
 
-import com.scheduling.Task;
-
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * FCFS Scheduling : First-Come First-Served
@@ -11,15 +10,45 @@ import java.util.List;
  * @since 20.05.05
  */
 public class Scheduling {
+    private final PriorityQueue<Task> taskList;
+    private final int size;
+    private int totalTurnAroundTime;
+    private int totalWaitingTime;
 
-    public void analyze(List<Task> taskList) {
+    public Scheduling(List<Task> taskList) {
+        this.taskList = new PriorityQueue<>(taskList);
+        this.size = taskList.size();
+    }
+
+    public void analyze() {
+        if (taskList.isEmpty()) {
+            return;
+        }
+
+        int elapsed = taskList.peek().getArriveTime();
+
+        while (!taskList.isEmpty()) {
+            Task task = taskList.poll();
+            elapsed += task.getBurstTime() +
+                    ((task.getArriveTime() - elapsed <= 0)
+                            ? 0
+                            : task.getArriveTime());
+
+            int turnAroundTime = elapsed - task.getArriveTime();
+            int waitTime = turnAroundTime - task.getBurstTime();
+
+            totalTurnAroundTime += turnAroundTime;
+            totalWaitingTime += waitTime;
+        }
     }
 
     public String getAverageTurnAroundTime() {
-        return null;
+        double avgTurnAroundTime = (double) totalTurnAroundTime / size;
+        return String.format("%.2f", avgTurnAroundTime);
     }
 
     public String getAverageWaitingTime() {
-        return null;
+        double avgWaitingTime = (double) totalWaitingTime / size;
+        return String.format("%.2f", avgWaitingTime);
     }
 }
--- src/test/java/com/scheduling/FCFS/FCFSTest.java
+++ src/test/java/com/scheduling/FCFS/FCFSTest.java
@@ -1,8 +1,6 @@
 package com.scheduling.FCFS;
 
-import com.scheduling.Task;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -19,34 +17,31 @@ import static org.hamcrest.CoreMatchers.is;
 public class FCFSTest {
     private Scheduling scheduling;
 
-    @Before
-    public void setUp() {
-        scheduling = new Scheduling();
-    }
-
     @Test
     public void 예제1() {
         List<Task> taskList = new ArrayList<>();
         taskList.add(new Task(0, 21));
         taskList.add(new Task(1, 3));
-        taskList.add(new Task(1, 6));
+        taskList.add(new Task(2, 6));
 
-        scheduling.analyze(taskList);
+        scheduling = new Scheduling(taskList);
+        scheduling.analyze();
 
-        Assert.assertThat(scheduling.getAverageTurnAroundTime(), is("25.00"));
-        Assert.assertThat(scheduling.getAverageWaitingTime(), is("15.00"));
+        Assert.assertThat(scheduling.getAverageTurnAroundTime(), is("24.00"));
+        Assert.assertThat(scheduling.getAverageWaitingTime(), is("14.00"));
     }
 
     @Test
     public void 예제2() {
         List<Task> taskList = new ArrayList<>();
+        taskList.add(new Task(2, 3));
         taskList.add(new Task(0, 21));
         taskList.add(new Task(1, 3));
-        taskList.add(new Task(1, 3));
 
-        scheduling.analyze(taskList);
+        scheduling = new Scheduling(taskList);
+        scheduling.analyze();
 
-        Assert.assertThat(scheduling.getAverageTurnAroundTime(), is("24.00"));
-        Assert.assertThat(scheduling.getAverageWaitingTime(), is("17.00"));
+        Assert.assertThat(scheduling.getAverageTurnAroundTime(), is("23.00"));
+        Assert.assertThat(scheduling.getAverageWaitingTime(), is("14.00"));
     }
 }
