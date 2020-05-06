bf7221095ccb73766359f09023174802e51dd311
keplor <keplor@naver.com>
2020-05-06 13:05:03 +0900

    Add FCFS disk scheduling test

---
+++ src/main/java/com/disk/FCFS/Scheduling.java
@@ -0,0 +1,21 @@
+package com.disk.FCFS;
+
+/**
+ * 디스크 스케줄링 FCFS (First-come First-Served)
+ *
+ * @author 송훤출
+ * @since 20.05.06
+ */
+public class Scheduling {
+
+
+    public Scheduling(int init_head, int[] queue) {
+    }
+
+    public void execute() {
+    }
+
+    public int getHeadDistance() {
+        return 0;
+    }
+}
