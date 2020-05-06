ea7ba7122a4e14af66cf3419e70b2b3f24656171
keplor <keplor@naver.com>
2020-05-06 15:26:35 +0900

    Add SSTF disk scheduling test

---
+++ src/main/java/com/disk/SSTF/Scheduling.java
@@ -0,0 +1,19 @@
+package com.disk.SSTF;
+
+/**
+ * SSTF (Shortest Seek Time First) disk scheduling
+ *
+ * @author 송훤출
+ * @since 20.05.06
+ */
+public class Scheduling {
+    public Scheduling(int init_head, int[] queue) {
+    }
+
+    public void execute() {
+    }
+
+    public int getHeadDistance() {
+        return 0;
+    }
+}
