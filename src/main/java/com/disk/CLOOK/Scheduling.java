9fa87945c9e18543ec8aceecd889c27bc11a05d2
keplor <keplor@naver.com>
2020-05-06 20:19:11 +0900

    Add C-LOOK disk scheduling algorithm test

---
+++ src/main/java/com/disk/CLOOK/Scheduling.java
@@ -0,0 +1,19 @@
+package com.disk.CLOOK;
+
+/**
+ * C-LOOK Disk Scheduling
+ *
+ * @author 송훤출
+ * @since 20.05.06
+ */
+public class Scheduling {
+    public Scheduling(int init_head, int[] queue) {
+    }
+
+    public void execute() {
+    }
+
+    public int getHeadDistance() {
+        return 0;
+    }
+}
